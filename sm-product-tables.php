196aad9f063098eca75fddeb67b398173c754c35
Marco Aspeitia <maspeitia.31@gmail.com>
2021-03-30 17:46:33 -0600

    Firts commit, basic structure of our plugin

---
+++ sm-product-tables.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: Stiles Machinery Product Tables
+Plugin URI: https://devitm.com
+Description: Customize WordPress with powerful, professional and intuitive fields.
+Version: 1.0.0
+Author: Marco Aspeitia
+Author URI: https://www.devitm.com
+Text Domain: sm-product-tables
+*/
+
+
+if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+if( ! function_exists('sm_product_tables') ) :
+
+    /* To implement de shortcode please write the next code [sm_product_tables_shortcode] in a DIVI code module */
+    function sm_product_tables(){
+        echo 'Hola desde el shortcode';
+    }
+    add_shortcode( 'sm_product_tables_shortcode', 'sm_product_tables', 10 );
+endif;
