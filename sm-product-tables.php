1c68e253df98b14d0416cb6b39bb298dc358d50b
Marco Aspeitia <maspeitia.31@gmail.com>
2021-03-30 19:42:45 -0600

    Fixing shortcode html displaying before divi builder content

--- sm-product-tables.php
+++ sm-product-tables.php
@@ -15,6 +15,8 @@ if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 if( ! function_exists('sm_product_tables') ) :
     /* To implement de shortcode please write the next code [sm_product_tables_shortcode] in a DIVI code module */
     function sm_product_tables(){
+        // begin output buffering
+        ob_start();
         $table = get_field('tabla_de_especificaciones'); 
         if($table){
         ?>
@@ -42,6 +44,8 @@ if( ! function_exists('sm_product_tables') ) :
         else{
             echo 'no hay contenido en la tabla de espeficicaciones de la maquinaria';
         }
+        // end output buffering, grab the buffer contents, and empty the buffer
+        return ob_get_clean();
     }
     add_shortcode( 'sm_product_tables_shortcode', 'sm_product_tables', 10 );
 endif;
