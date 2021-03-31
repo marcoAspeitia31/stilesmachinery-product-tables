51e344c01554788f679f487453b36a7a01ae241c
Marco Aspeitia <maspeitia.31@gmail.com>
2021-03-30 19:16:23 -0600

    Creating the table content

--- sm-product-tables.php
+++ sm-product-tables.php
@@ -13,13 +13,35 @@ Text Domain: sm-product-tables
 if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 if( ! function_exists('sm_product_tables') ) :
-
     /* To implement de shortcode please write the next code [sm_product_tables_shortcode] in a DIVI code module */
     function sm_product_tables(){
-        echo '<pre>';
-        var_dump(get_field('tabla_de_especificaciones'));
-        echo '<pre>';
-        echo 'Hola desde el shortcode';
+        $table = get_field('tabla_de_especificaciones'); 
+        if($table){
+        ?>
+        <table>
+            <thead>
+                <tr>
+                    <?php foreach($table['header'] as $th): ?>
+
+                    <th><?php echo $th['c']; ?></th>
+                    <?php endforeach; ?>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($table['body'] as $tr): ?>
+                <tr>
+                    <?php foreach($tr as $td): ?>
+                    <td><?php echo $td['c']; ?></td>
+                    <?php endforeach; ?>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+        <?php
+        }
+        else{
+            echo 'no hay contenido en la tabla de espeficicaciones de la maquinaria';
+        }
     }
     add_shortcode( 'sm_product_tables_shortcode', 'sm_product_tables', 10 );
 endif;
