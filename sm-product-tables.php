283933cf21891032ef324bbd9cbc70916b3040e8
Marco Aspeitia <maspeitia.31@gmail.com>
2021-04-06 22:36:23 -0500

    Code documentation

--- sm-product-tables.php
+++ sm-product-tables.php
@@ -12,20 +12,23 @@ Text Domain: sm-product-tables
 
 if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-
+/**
+ * Custom Shortcode of machinery product table compatible with ACF and ACF Table Field
+ */
 if( ! function_exists('sm_product_tables') ) :
-    /* To implement de shortcode please write the next code [sm_product_tables_shortcode] in a DIVI code module */
+
     function sm_product_tables(){
         // begin output buffering
         ob_start();
+
         $table = get_field('tabla_de_especificaciones'); 
         if($table){
         ?>
+
         <table class="machinery-table">
             <thead>
                 <tr>
                     <?php foreach($table['header'] as $th): ?>
-
                     <th><?php echo $th['c']; ?></th>
                     <?php endforeach; ?>
                 </tr>
@@ -64,26 +67,39 @@ if( ! function_exists('sm_product_tables') ) :
                 ?>
             </tbody>
         </table>
+
         <?php
         }
         else{
             echo 'no hay contenido en la tabla de espeficicaciones de la maquinaria';
         }
+
         // end output buffering, grab the buffer contents, and empty the buffer
         return ob_get_clean();
+
     }
     add_shortcode( 'sm_product_tables_shortcode', 'sm_product_tables', 10 );
     /* To implement de shortcode please write the next code [sm_product_tables_shortcode] in a DIVI code module */
-endif;
 
+endif; // function_exists('sm_product_tables)
+
+/**
+ * Custom Shortcode of machinery gallery slider compatible with slick slider and CMB2 custom fields
+ */
 if( ! function_exists('sm_product_gallery') ) :
 
     function sm_product_gallery(){
+
         // begin output buffering
         ob_start();
+
         //printf ('<pre>%s</pre>', var_export(get_post_custom( get_the_ID()), true) );
         $machinery_gallery = get_post_meta(get_the_ID(), 'machinery_product_galeria_imagenes' , true);
+
         if (!empty($machinery_gallery)){
+            /**
+             * First Slider for machinery images
+             */
             echo '<ul class="machinery-gallery">';
                 foreach($machinery_gallery as $id => $image){
                     echo '<li class="image-slide">';
@@ -91,6 +107,10 @@ if( ! function_exists('sm_product_gallery') ) :
                     echo '</li>';
                 }
             echo '</ul>';
+
+            /**
+             * Second Slider for machinery caption
+             */
             echo '<ul class="machinery-caption">';
                 foreach($machinery_gallery as $id => $image){
                     $image_alt = get_post_meta( $id, '_wp_attachment_image_alt', true);
@@ -100,10 +120,12 @@ if( ! function_exists('sm_product_gallery') ) :
                 }
             echo '</ul>';
         }
+
         // end output buffering, grab the buffer contents, and empty the buffer
         return ob_get_clean();
+
     }
     add_shortcode( 'sm_product_gallery_shortcode', 'sm_product_gallery', 10 );
     /* To implement de shortcode please write the next code [sm_product_gallery_shortcode] in a DIVI code module */
 
-endif;
+endif; //function_exists('sm_product_gallery')
